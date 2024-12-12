11ddab4749efc2c2c3d3e9075943aa62a9256331
Sagantpss <sagantpss@gmail.com>
2024-12-12 17:25:46 +0530

    fist commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import { Button } from 'react-bootstrap';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Ecomm Dashboard</h1>
+      <Button variant="primary">Button</Button>
+    </div>
+  );
+}
+
+export default App;
